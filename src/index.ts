5e87998ccd52c15ae9aee6082fb8ee6446fd3f89
SatooRu65536 <ryuto65536@gmail.com>
2026-03-29 02:12:14 +0900

    init: slack bot setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import type { SlackEdgeAppEnv } from 'slack-cloudflare-workers';
+import { SlackApp } from 'slack-cloudflare-workers';
+
 export default {
-  async fetch(_request, _env, _ctx): Promise<Response> {
-    return new Response('Hello World!');
+  async fetch(
+    request: Request,
+    env: SlackEdgeAppEnv,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
+    const app = new SlackApp({ env });
+
+    app.command('/health-check', async () => 'Hi! I am healthy!');
+
+    return await app.run(request, ctx);
   },
-} satisfies ExportedHandler<Env>;
+};
