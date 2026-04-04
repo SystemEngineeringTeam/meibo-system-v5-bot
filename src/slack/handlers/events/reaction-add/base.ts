3d278adeb730480ad56caa237a2b066383a2686d
SatooRu65536 <ryuto65536@gmail.com>
2026-04-05 07:45:49 +0900

    Revert "feat: reaction-add event"
    This reverts commit 8eb33d00b80dae7a2466b1bacae5af1202324c6c.

--- src/slack/handlers/events/reaction-add/base.ts
+++
@@ -1,7 +0,0 @@
-import type { EventLazyHandler } from 'slack-cloudflare-workers';
-import type { HonoSlackAppEnv } from '@/types/hono';
-
-export const reactionAddEventHandlerBase = (reaction: string, lazy: EventLazyHandler<'reaction_added', HonoSlackAppEnv>): EventLazyHandler<'reaction_added', HonoSlackAppEnv> => async (req) => {
-  if (reaction !== req.payload.reaction) return;
-  await lazy(req);
-};
--- src/slack/handlers/events/reaction-add/index.ts
+++
@@ -1,8 +0,0 @@
-import type { EventLazyHandler } from 'slack-cloudflare-workers';
-import type { HonoSlackAppEnv } from '@/types/hono';
-import { addApproveReactionHandler } from './add-approve-reaction';
-import { addRejectReactionHandler } from './add-reject-reaction';
-
-export const reactionAddEventHandler: EventLazyHandler<'reaction_added', HonoSlackAppEnv> = async (req) => {
-  await Promise.all([]);
-};
