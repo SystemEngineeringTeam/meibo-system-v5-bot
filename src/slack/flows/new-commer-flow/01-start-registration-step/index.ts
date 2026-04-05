6761d7535948996a2be0919a4ee601b428acc3df
SatooRu65536 <ryuto65536@gmail.com>
2026-04-05 11:36:08 +0900

    faet: ログインURLに有効期限を設定

--- src/slack/flows/new-commer-flow/01-start-registration-step/index.ts
+++ src/slack/flows/new-commer-flow/01-start-registration-step/index.ts
@@ -1,13 +1,17 @@
+import type { Dayjs } from 'dayjs';
 import type { SlackAppContext } from 'slack-cloudflare-workers';
 import type { HonoSlackAppEnv } from '@/types/hono';
 import type { ChannelData, LinkData } from '@/types/kv';
+import dayjs from 'dayjs';
 import { v4 as uuidv4 } from 'uuid';
 import { kv } from '@/utils/kv';
 
 export const startRegistrationStep = async (slackUserId: string, context: SlackAppContext, env: HonoSlackAppEnv) => {
   // ユーザIDと紐づく一意なキー
   const key = uuidv4();
-  await kv.put<LinkData>(env.LINK_KV, key, { slackUserId });
+
+  const expiration = dayjs().add(10, 'minute').second(59); // 10分で期限切れ
+  await kv.put<LinkData>(env.LINK_KV, key, { slackUserId }, { expiration: expiration.unix() });
 
   // ログインURLを生成
   const loginUrl = new URL('/login', env.SLACK_BOT_BASE_URL!);
@@ -29,7 +33,7 @@ export const startRegistrationStep = async (slackUserId: string, context: SlackA
       // メッセージを送信
       context.client.chat.postMessage({
         channel: channelId,
-        text: generateText(loginUrl),
+        text: generateText(loginUrl, expiration),
         mrkdwn: true,
       }),
     ]);
@@ -38,11 +42,12 @@ export const startRegistrationStep = async (slackUserId: string, context: SlackA
   }
 };
 
-function generateText(loginUrl: URL): string {
+function generateText(loginUrl: URL, expiration: Dayjs): string {
   return `*【重要】部員登録をしてください* – 名簿管理システム
 
 *STEP 1: 下記リンクより Gmail アカウントを紐づけてください
 ログインURL: ${loginUrl.toString()}
+有効期限: 10分 (${expiration.format('MM/DD HH:mm')})
 
 ※このURLは一時的なもので、他の人と共有しないでください。`;
 }
